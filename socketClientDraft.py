07c0a44a76193a4bd07ec659a2dab759c3df9e3b
weywocketer <pawelbiesikirski@gmail.com>
2019-05-27 23:37:05 +0200

    played a bit with sockets drafts

--- socketClientDraft.py
+++ socketClientDraft.py
@@ -5,9 +5,10 @@ import socket
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(input("wpisz wiadomość do wysłania.. .. .. ").encode())
+data = s.recv(1024)
 
-print('Received', repr(data))
+print('Received', data.decode())
+s.close()
